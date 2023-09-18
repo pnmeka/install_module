ad14e5cdc237b3c11be1f604fc66bab29c6a2b7d
Meka <82007659+pnmeka@users.noreply.github.com>
2023-09-18 06:17:15 -0400

    Delete install_module directory

--- install_module/README.md
+++
@@ -1,2 +0,0 @@
-# install_module
-This is an install_module to be used for Darna.HI, a self custody of Health files app
--- install_module/XMRIG/Info.js
+++
@@ -1,8 +0,0 @@
-const appInfo = {
-    App_name: 'XMRIG', 
-    App_folder: 'XMRIG',
-    Port: '3007',
-    Requirements: 'Docker, XMR wallet address required. GO TO <IP_ADDRESS>:3007 to complete installtion process',
-    Description: "High performance, open source, cross platform RandomX, KawPow, CryptoNight and GhostRider CPU/GPU miner made by XMRig. It allows the user to mine XMR while running the server allowing you to monetize CPU time. However the earning is minimal. Running a miner could also slow the performance of your computer. The writer of this program isnt affiliated in anyway with XMRig.  "
-};
-
