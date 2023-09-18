8bae093de906ee505b0542f3d785d7476e1a82dc
Meka <82007659+pnmeka@users.noreply.github.com>
2023-09-18 06:13:42 -0400

    Add files via upload

---
+++ XMR/Info.js
@@ -0,0 +1,8 @@
+const appInfo = {
+    App_name: 'XMR Node', 
+    App_folder: 'XMR',
+    Port: '5000',
+    Requirements: 'Docker. 64 bit CPU. It wouldnt work of Single board computers.',
+    Description: "The use of Monero aligns with both the principles of personal security and the broader objectives of maintaining privacy within the healthcare landscape. Hence, Darna.HI sees value in it. That being said we have nothing to do with creating and maintaining the Monero ecosystem. This install sets up a Monero Node on your computer. It would likely need around 75 Gb to store the blockchain file. If you wish there is an installation option at the bottom of the page that allows you to install XMrig Miner."
+};
+
